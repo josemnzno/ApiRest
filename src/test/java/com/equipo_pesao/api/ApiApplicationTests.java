d1bde63bdc6e347522c29a968ffdfc8bd0057f0c
jmanzano <racidn78@gmail.com>
2025-02-20 15:50:00 -0600

    first commit

---
+++ src/test/java/com/equipo_pesao/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo_pesao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
